3c202694828bc404e47ee3787039e8a655d179a5
MiguelRosas11 <roguemijose@gmail.com>
2025-03-18 03:13:11 -0600

    Método para cargar los datos del csv, clase pokemon y clase usuario

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        String ruta = "\\C:\\Users\\rogue\\OneDrive\\Escritorio\\pokemon_data_pokeapi.csv\\";
         System.out.println( "Hello World!" );
     }
 }
