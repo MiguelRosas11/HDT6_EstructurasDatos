17f4de8dcd974fdfdd11f18b28717456ae97de10
MiguelRosas11 <roguemijose@gmail.com>
2025-03-18 11:54:02 -0600

    Clase pokedex y métodos finalizados, comienzo de clase main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        String ruta = "\\C:\\Users\\rogue\\OneDrive\\Escritorio\\pokemon_data_pokeapi.csv\\";
-        System.out.println( "Hello World!" );
+        System.out.println("Seleccione la implementación de Map para la Pokédex:");
+        System.out.println("1) HashMap  2) TreeMap  3) LinkedHashMap");
+        int choicePokedex = sc.nextInt();
+
+        Mapa mapaPokedex;
+        switch (choicePokedex) {
+            case 1:
+                mapaPokedex = new HashMapMapa();
+                break;
+            case 2:
+                mapaPokedex = new TreeMapMapa();
+                break;
+            case 3:
+                mapaPokedex = new LinkedHashMapMapa();
+                break;
+            default:
+                System.out.println("Opción inválida, se usará HashMap por defecto.");
+                mapaPokedex = new HashMapMapa();
+
+        }
+        int menu = 0;
+        while(menu<1) {
+            System.out.println("Bienvenido, ¿qué desea realizar? \n1. Agregar un pokemon a coleccion. \n2. Mostrar los datos de un pokemon ingresando su nombre. \n3. Mostrar el pokemones de la colección ordenados por tipo 1. \n4. Mostrar pokemones existentes ordenados por tipo 1. \n5. Buscar pokemones ingresando habilidad.");
+            int opc = sc.nextInt(); sc.nextLine();
+        }
     }
 }
---
+++ src/main/java/org/example/Pokedex.java
@@ -0,0 +1,72 @@
+package org.example;
+import java.io.*;
+import java.util.*;
+
+public class Pokedex {
+    private Mapa pokedex;
+
+    public Pokedex(Mapa pokedex) {
+        this.pokedex = CargarPokemons(ruta, pokedex);
+    }
+
+    static String ruta = "C:/Users/rogue/OneDrive/Escritorio/pokemon_data_pokeapi.csv";
+
+    public Mapa CargarPokemons(String ruta, Mapa pokedex) {
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                String name = data[0];
+                int Number = Integer.parseInt(data[1]);
+                String type1 = data[2];
+                String type2 = data[3].isEmpty() ? "None" : data[3];
+                String classification = data[4];
+                double height = Double.parseDouble(data[5]);
+                double weight = Double.parseDouble(data[6]);
+                List<String> abilities = Arrays.asList(data[7].split(", "));
+                int generation = Integer.parseInt(data[8]);
+                boolean isLegendary = data[9].equalsIgnoreCase("Yes");
+
+                Pokemon pokemon = new Pokemon(name, Number, type1, type2, classification,
+                        height, weight, abilities, generation, isLegendary);
+                pokedex.put(name, pokemon);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+        return pokedex;
+    }
+
+    public void mostrarTodosOrdenadosPorTipo() {
+        List<Pokemon> listaPokemon = new ArrayList<>(pokedex.mapaInterno.values());
+        Collections.sort(listaPokemon, new Comparator<Pokemon>() {
+            @Override
+            public int compare(Pokemon p1, Pokemon p2) {
+                return p1.getType1().compareTo(p2.getType1());
+            }
+        });
+
+        for (Pokemon p : listaPokemon) {
+            System.out.println(p.getName() + " - " + p.getType1());
+        }
+    }
+
+
+    public void buscarPorHabilidad(String habilidad) {
+        boolean encontrado = false;
+
+        for (Pokemon p : pokedex.mapaInterno.values()) {
+            if (p.getAbilities().contains(habilidad)) {
+                System.out.println(p.getName());
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("No se encontraron Pokémon con la habilidad: " + habilidad);
+        }
+    }
+
+
+}
