8e27482efa63519d6537f854d7fc3a38170ba41d
MiguelRosas11 <roguemijose@gmail.com>
2025-03-18 17:19:32 -0600

    Estructura main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,7 +12,7 @@ public class App
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Seleccione la implementación de Map para la Pokédex:");
-        System.out.println("1) HashMap  2) TreeMap  3) LinkedHashMap");
+        System.out.println("1. HashMap  \n2. TreeMap  \n3. LinkedHashMap");
         int choicePokedex = sc.nextInt();
 
         Mapa mapaPokedex;
@@ -35,6 +35,32 @@ public class App
         while(menu<1) {
             System.out.println("Bienvenido, ¿qué desea realizar? \n1. Agregar un pokemon a coleccion. \n2. Mostrar los datos de un pokemon ingresando su nombre. \n3. Mostrar el pokemones de la colección ordenados por tipo 1. \n4. Mostrar pokemones existentes ordenados por tipo 1. \n5. Buscar pokemones ingresando habilidad.");
             int opc = sc.nextInt(); sc.nextLine();
+            if(opc == 1){
+
+            }
+            else if (opc == 2){
+
+            }
+
+            else if (opc == 3){
+
+            }
+
+            else if (opc == 4){
+
+            }
+
+            else if (opc == 5){
+
+            }
+
+            else if (opc == 6){
+                System.out.println("Gracias por utilizar el programa");
+                menu += 1;
+            }
+            else {
+                System.out.println("Por favor introduzca una opción válida");
+            }
         }
     }
 }
